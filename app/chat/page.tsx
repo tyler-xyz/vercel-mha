9074c06b93a5d99b66f7aabc93ef6a87239510d4
tyler-xyz <t13productions@gmail.com>
2023-09-22 07:33:21 -0400

    staged

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -10,7 +10,7 @@ export default function Chat() {
   return (
     <>
 {/* box container for chatWindow demo */}
-    <div className="box-content my-10 w-full max-w-screen-xl gap-5 px-5 xl:px-0 border-2">
+    {/* <div className="box-content my-10 w-full max-w-screen-xl gap-5 px-5 xl:px-0 border-2"> */}
 {/* temporary nav warning */}
 <div className="alert alert-info bg-indigo-500/50 mx-auto mt-6 items-center justify-center space-x-5 ">
            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="stroke-current shrink-0 w-6 h-6"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
@@ -37,7 +37,7 @@ export default function Chat() {
 </div>
 {/* text input temporary */}
 <input type="text" placeholder="Type your message here" className="input input-bordered w-full max-w-xs" />
-    </div>
+    {/* </div> */}
     </>
   );
 }

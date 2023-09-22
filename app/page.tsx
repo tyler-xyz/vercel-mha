143e2863c0f97cbbaae1636112fff9e31c02c032
tyler-xyz <t13productions@gmail.com>
2023-09-22 05:37:58 -0400

    style

--- app/page.tsx
+++ app/page.tsx
@@ -81,7 +81,7 @@ export default async function Home() {
           Please note that while you chat with us you must remain on the website. If you navigate away from the chat window your connection may be lost.
           </Balancer>
         </p>  */}
-        <div className="alert alert-info mx-auto mt-6 flex animate-fade-up items-center justify-center space-x-5 ">
+        <div className="alert alert-info bg-sky-500/50 mx-auto mt-6 flex animate-fade-up items-center justify-center space-x-5 ">
            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="stroke-current shrink-0 w-6 h-6"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
             <span>Please note that while you chat with us you must remain on the website. If you navigate away from the chat window your connection may be lost.</span>
         </div>

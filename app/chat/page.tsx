0e870b8d58639f365564ab564f460b19a2bfcf85
tyler-xyz <t13productions@gmail.com>
2023-09-22 06:41:26 -0400

    dummy input with daisyui

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -9,9 +9,23 @@ import { Box } from "@twilio-paste/box";
 export default function Chat() {
   return (
     <>
-      <div className="my-10 grid w-full max-w-screen-xl animate-fade-up grid-cols-1 gap-5 px-5 md:grid-cols-1 xl:px-0">
+{/* box container for chatWindow demo */}
+    <div className="box-content my-10 grid w-full max-w-screen-xl animate-fade-up grid-cols-1 gap-5 px-5 md:grid-cols-1 xl:px-0 border-4">
+{/* temporary chatbubbles */}
+    <div className="chat chat-start">
+    <div className="chat-bubble">You were the Chosen One!</div>
+    <div className="chat-footer opacity-50">
+      Seen
+  </div>
+</div>
+<div className="chat chat-end">
+  <div className="chat-bubble">I loved you.</div>
+  <div className="chat-footer opacity-50">
+    Delivered
+  </div>
+</div>
 
-      </div>
+    </div>
     </>
   );
 }

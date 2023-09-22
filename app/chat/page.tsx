16330eb3a282798cc43a6d08708e1c47417ea3e3
tyler-xyz <t13productions@gmail.com>
2023-09-22 08:20:54 -0400

    sizing

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -13,7 +13,7 @@ export default function Chat() {
 <div className="z-10 w-full max-w-xl px-5 xl:px-0">
 {/* temporary nav warning */}
 <div className="alert alert-info bg-indigo-500/50">
-<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="stroke-current shrink-0 w-6 h-6"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="stroke-current shrink-0 w-4 h-4"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
   <p className="md:text-xsm">Please note. If you navigate away from the chat window your connection may be lost.</p>
 </div>
 

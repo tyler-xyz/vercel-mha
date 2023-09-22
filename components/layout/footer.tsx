bd2cd50ed17fae46373eaa958064ff3c5126ec0e
tyler-xyz <t13productions@gmail.com>
2023-09-21 20:31:38 -0400

    c/p footer.tsx comp/layout

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -1,15 +1,32 @@
+const styles = {
+  footer: {
+    backgroundColor: "#462d91",
+  },
+};
 export default function Footer() {
   return (
-    <div className="absolute w-full border-t border-gray-200 bg-white py-5 text-center">
-      <p className="text-gray-500">
-        A free template by{" "}
+    <div className="absolute w-full border-t border-gray-200 bg-opacity-100 bg-purple py-5 text-center">
+      <p className="text-white">
+        Need Additional Help?{" "}
         <a
-          className="font-medium text-gray-800 underline transition-colors"
-          href="https://twitter.com/steventey"
-          target="_blank"
+          className="font-medium text-white underline transition-colors inline-flex"
+          href="https://justtellone.org/about/"
+          target="_self"
           rel="noopener noreferrer"
         >
-          Steven Tey
+          Read more on our website 
+          <svg 
+            xmlns="http://www.w3.org/2000/svg" 
+            fill="none" 
+            viewBox="0 0 24 24" 
+            stroke-width="1.5" 
+            stroke="currentColor" className="w-4 h-4 inline-flex">
+          <path 
+            stroke-linecap="round" 
+            stroke-linejoin="round" 
+            d="M13.5 6H5.25A2.25 2.25 0 003 8.25v10.5A2.25 2.25 0 005.25 21h10.5A2.25 2.25 0 0018 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25" 
+          />
+          </svg>
         </a>
       </p>
     </div>

a57a6f1db857d1704ac9123c72bae7514d0c5983
tyler-xyz <t13productions@gmail.com>
2023-09-22 07:41:42 -0400

    /chatpage

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -10,9 +10,9 @@ export default function Chat() {
   return (
     <>
 {/* box container for chatWindow demo */}
-    {/* <div className="box-content my-10 w-full max-w-screen-xl gap-5 px-5 xl:px-0 border-2"> */}
+ <div className="box-content my-10 w-full max-w-screen-xl gap-5 px-5 xl:px-0 border-2"> */}
 {/* temporary nav warning */}
-<div className="alert alert-info bg-indigo-500/50 mx-auto mt-6 items-center justify-center space-x-5 ">
+<div className="alert alert-info bg-indigo-500/50 items-center justify-center ">
            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="stroke-current shrink-0 w-6 h-6"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
             <p className="md:text-xsm">Please note. If you navigate away from the chat window your connection may be lost.</p>
 </div>
@@ -24,20 +24,20 @@ export default function Chat() {
 </div>
 </div>
     <div className="chat chat-start">
-    <div className="chat-bubble">You were the Chosen One!</div>
+    <div className="chat-bubble">No</div>
     <div className="chat-footer opacity-50">
       Seen
   </div>
 </div>
 <div className="chat chat-start">
-  <div className="chat-bubble">I loved you.</div>
+  <div className="chat-bubble">14228</div>
   <div className="chat-footer opacity-50">
     Delivered
   </div>
 </div>
 {/* text input temporary */}
 <input type="text" placeholder="Type your message here" className="input input-bordered w-full max-w-xs" />
-    {/* </div> */}
+</div>
     </>
   );
 }

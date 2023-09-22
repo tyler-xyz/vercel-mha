09c93594ad03575120d0da1a06bf25ddb4f061bf
tyler-xyz <t13productions@gmail.com>
2023-09-21 20:37:36 -0400

    c/p navbar

--- components/layout/navbar.tsx
+++ components/layout/navbar.tsx
@@ -15,7 +15,7 @@ export default function NavBar({ session }: { session: Session | null }) {
     <>
       <SignInModal />
       <div
-        className={`fixed top-0 w-full flex justify-center ${
+        className={`fixed top-0 w-full flex justify-center border-b border-gray-200 ${
           scrolled
             ? "border-b border-gray-200 bg-white/50 backdrop-blur-xl"
             : "bg-white/0"
@@ -25,24 +25,15 @@ export default function NavBar({ session }: { session: Session | null }) {
           <Link href="/" className="flex items-center font-display text-2xl">
             <Image
               src="/logo.png"
-              alt="Precedent logo"
-              width="30"
-              height="30"
+              alt="JustTellOne logo"
+              width="110"
+              height="60"
               className="mr-2 rounded-sm"
-            ></Image>
-            <p>Precedent</p>
+            >
+            </Image>
+            {/* <p>Precedent</p> */}
           </Link>
           <div>
-            {session ? (
-              <UserDropdown session={session} />
-            ) : (
-              <button
-                className="rounded-full border border-black bg-black p-1.5 px-4 text-sm text-white transition-all hover:bg-white hover:text-black"
-                onClick={() => setShowSignInModal(true)}
-              >
-                Sign In
-              </button>
-            )}
           </div>
         </div>
       </div>

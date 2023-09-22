669734dacc2c012656d192531e70bae9b3e30163
tyler-xyz <t13productions@gmail.com>
2023-09-22 06:58:25 -0400

    elminiate fade on div

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -10,7 +10,7 @@ export default function Chat() {
   return (
     <>
 {/* box container for chatWindow demo */}
-    <div className="box-content my-10 grid w-full max-w-screen-xl animate-fade-up grid-cols-1 gap-5 px-5 md:grid-cols-1 xl:px-0 border-4">
+    <div className="box-content my-10 grid w-full max-w-screen-xl grid-cols-1 gap-5 px-5 md:grid-cols-1 xl:px-0 border-4">
 {/* temporary nav warning */}
 <div className="alert alert-info bg-indigo-500/50 mx-auto mt-6 flex animate-fade-up items-center justify-center space-x-5 ">
            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="stroke-current shrink-0 w-6 h-6"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>

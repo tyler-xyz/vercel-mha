50cbd8361a6043f4f8b181b3e4777c22df7edd25
tyler-xyz <t13productions@gmail.com>
2023-09-29 01:19:05 -0400

    new_convo_hook,  new components for convo-app

--- components/twilio/convo-app.tsx
+++ components/twilio/convo-app.tsx
@@ -2,7 +2,7 @@ import { Theme } from "@twilio-paste/core/dist/theme";
 import { Box, Grid, Column } from "@twilio-paste/core";
 import { useState, useEffect } from "react";
 import { Client as TwilioClient } from "@twilio/conversations";
-
+import { Message, User, Conversation, Participant   } from "@twilio/conversations";
 //full front-end app
 
 

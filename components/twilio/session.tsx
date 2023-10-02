4769a12b0ca2b5efeee9eef1876a8e13557c0655
tyler-xyz <t13productions@gmail.com>
2023-10-01 21:55:00 -0400

    session - connects to api for token and chatgrant

--- components/twilio/session.tsx
+++ components/twilio/session.tsx
@@ -1,8 +1,7 @@
 import twilio from 'twilio';
-import { Client as TwilioClient, Conversation, User,
-} from "@twilio/conversations";
+import AccessToken from 'twilio/lib/jwt/AccessToken';
 
-// apply permanent twilio Vars
+// apply permanent twilio Vars from .env
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
 const twilioApiKey = process.env.TWILIO_API_KEY;
@@ -12,18 +11,24 @@ const serviceSid = process.env.TWILIO_CHAT_SERVICE_SID;
 const identity = 'Anonymous';
 //added flowSid for stored value
 const flowSid = process.env.STUDIO_FLOW_SID;
-
-//connect to the conversation via webhook
-export default async function Session(
-    accountSid,
-    authToken,
-    twilioApiKey,
-    twilioApiSecret,
-    serviceSid,
-    identity,
-    flowSid
-) {
 //connect to twilio acct client
 const client = require('twilio')(accountSid, authToken);
 
+//connect to the conversation via webhook
+export default function getToken() {
+    const AcessToken = require('twilio').jwt.AccessToken;
+    const ChatGrant = AccessToken.ChatGrant;
+    //issue chatGrant
+    const chatGrant = new ChatGrant({
+        serviceSid: serviceSid,
+    });
+    //
+    const token = new AcessToken(
+        accountSid,
+        twilioApiKey,
+        twilioApiSecret,
+        identity
+    );
+    token.addGrant(chatGrant);
+    return token.toJwt();
 }

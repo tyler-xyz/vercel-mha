1ad8b591f8f34efb854627e6b94f2b1c88b12cf3
tyler-xyz <t13productions@gmail.com>
2023-10-03 14:48:05 -0400

    new functions added. 5 errors

--- components/twilio/convo-app.tsx
+++ components/twilio/convo-app.tsx
@@ -1,27 +1,61 @@
 import { Theme } from "@twilio-paste/core/dist/theme";
 import axios from "axios";
-import createSession from "@/app/session";
 import twilio from 'twilio';
+import React, { useState, useEffect } from "react";
+import createSession from "@/app/session";
+import { Message } from '@twilio/conversations';
 import { Box, Grid, Column } from "@twilio-paste/core";
-import { useState, useEffect } from "react";
-import { Client, } from "@twilio/conversations";
-import { Message, User, Conversation, Participant   } from "@twilio/conversations";
+import { User, Conversation, Participant   } from "@twilio/conversations";
+import { Client } from "@twilio/conversations";
 
 //assign perm vars
     const serviceSid = process.env.TWILIO_CHAT_SERVICE_SID; 
-    const flowSid = process.env.STUDIO_FLOW_SID;; 
+    const flowSid = process.env.STUDIO_FLOW_SID;
+    const syncSid = process.env.TWILIO_SYNC_SERVICE_SID; 
 
-export async function ConversationsApp(){
-    
-    //connect to twilio acct client
-    const client = twilio({ accountSid: twilioAcctSid },twilioApiKey, twilioApiSecret);
-    
-    //get token using axios from session.tsx
-    async function chatGrant(identity){
-        const response = await axios.get('./app/session.tsx');  
-    return response.data;
+//get token using axios from session.tsx
+    async function chatGrant() {
+        try {
+            const response = await axios.get('./app/session.js');
+            console.log('Response Data:', response.data); // see response data
+            return response.data;
+            } catch (error) {
+                console.error('Error fetching token:', error);
+            }
+        }
+
+//get client from chatGrant token
+    async function getClient(){
+        const { accountSid, apiKey, apiSecret } = await chatGrant();
+        const client = new Client({ accountSid , apiKey, apiSecret });
+        return client;
+        }
+
+//get conversation from client
+    async function getConversation(client) {
+        const conversation = await client.getConversationByUniqueName('conversation-unique-name').fetch();
+        return conversation;
     }
-    
+
+// get messages from conversation
+    async function getMessages(conversation){
+        const messages = await conversation.getMEssages();
+        return messages;
+    }
+
+// send message to conversation
+    async function sendMessage(conversation, body) {
+        await conversation.sendMessage(body);
+    }
+
+//begin ConversationsApp        
+const ConversationsApp = () => {
+    const [client, setClient] = useState(null);
+    const [conversation, setConversation] = useState(null);
+    const [messages, setMessages] = useState([]);
+
+
+//ConversationApp return
     return(
     <Theme.Provider theme="twilio">
 

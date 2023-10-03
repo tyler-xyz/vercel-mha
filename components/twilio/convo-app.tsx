eab60e315d25e424ec2dd1c82dca27340c1232f8
tyler-xyz <t13productions@gmail.com>
2023-10-03 01:29:15 -0400

    updates to convo-app

--- components/twilio/convo-app.tsx
+++ components/twilio/convo-app.tsx
@@ -1,9 +1,10 @@
 import { Theme } from "@twilio-paste/core/dist/theme";
 import axios from "axios";
 import createSession from "@/app/session";
+import twilio from 'twilio';
 import { Box, Grid, Column } from "@twilio-paste/core";
 import { useState, useEffect } from "react";
-import { Client as TwilioClient } from "@twilio/conversations";
+import { Client, } from "@twilio/conversations";
 import { Message, User, Conversation, Participant   } from "@twilio/conversations";
 
 //assign perm vars
@@ -16,12 +17,10 @@ export async function ConversationsApp(){
     const client = twilio({ accountSid: twilioAcctSid },twilioApiKey, twilioApiSecret);
     
     //get token using axios from session.tsx
-async function chatGrant(){
-    const response = await axios.get('./app/session.tsx')  
-return 
-}
-
-    //initialize conversation
+    async function chatGrant(identity){
+        const response = await axios.get('./app/session.tsx');  
+    return response.data;
+    }
     
     return(
     <Theme.Provider theme="twilio">

bad5ebb4c2990ddb9a571248b77c55dfa62ebcdc
tyler-xyz <t13productions@gmail.com>
2023-10-05 17:00:14 -0400

    added separator

--- app/chat/app.tsx
+++ app/chat/app.tsx
@@ -1,14 +1,15 @@
 import { Box } from "@twilio-paste/box";
-import { Heading } from "@twilio-paste/core";
+import { Heading, Separator } from "@twilio-paste/core";
 import React from "react";
 import { CustomerChatLog } from "@/components/twilio/chat-log";
 
 export const App: React.FC = () => {
   return (
     <Box>
-     <Heading as="h4" variant="heading40">
+     <Heading as="h4" variant="heading50">
         Live Chat
       </Heading>
+      <Separator orientation="vertical" horizontalSpacing="space20" />
         <CustomerChatLog />
 
     </Box>

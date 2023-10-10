35849eb1d26dbccc1f8fde31943eafa59f2d0deb
tyler-xyz <77706863+tyler-xyz@users.noreply.github.com>
2023-10-10 03:49:35 +0000

    .env setup

--- session.js
+++ session.js
@@ -3,13 +3,13 @@ const ChatGrant = AccessToken.ChatGrant;
 
 // Used when generating any kind of tokens
 // To set up environmental variables, see http://twil.io/secure
-const twilioAccountSid = process.env.TWILIO_ACCOUNT_SID;
-const twilioApiKey = process.env.TWILIO_API_KEY;
-const twilioApiSecret = process.env.TWILIO_API_SECRET;
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const keySid = process.env.TWILIO_API_KEY;
+const secret = process.env.TWILIO_API_SECRET;
 
 // Used specifically for creating Chat tokens
 const serviceSid = process.env.TWILIO_CHAT_SERVICE_SID;
-const identity = 'user@example.com';
+const identity = 'anonymous';
 
 // Create a "grant" which enables a client to use Chat as a given user,
 // on a given device
@@ -20,9 +20,9 @@ const chatGrant = new ChatGrant({
 // Create an access token which we will sign and return to the client,
 // containing the grant we just created
 const token = new AccessToken(
-  twilioAccountSid,
-  twilioApiKey,
-  twilioApiSecret,
+  accountSid,
+  keySid,
+  secret,
   {identity: identity}
 );
 

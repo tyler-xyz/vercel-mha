1cbb148f0c0c2a5c8dfb3e36bede271061528dae
tyler-xyz <t13productions@gmail.com>
2023-10-11 23:36:30 -0400

    consolidating to server

--- app/api/server.ts
+++ app/api/server.ts
@@ -1,25 +1,17 @@
-import twilio from 'twilio';
-
-const AccessToken = twilio.jwt.AccessToken;
-const ChatGrant = AccessToken.ChatGrant;
-
+//
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
 const keySid = process.env.TWILIO_API_KEY;
 const secret = process.env.TWILIO_API_SECRET;
-// Used specifically for creating Chat tokens
 const serviceSid = process.env.TWILIO_CHAT_SERVICE_SID;
+//
+const AccessToken = require('twilio').jwt.AccessToken;
+const ChatGrant = AccessToken.ChatGrant;
 const identity = 'anonymous';
+const client = require('twilio')(accountSid, authToken)
 
 //begin newSession to gen and return token and identity
-export default async function newSession(
-  accountSid: string,
-  keySid: string,
-  secret: string,
-  identity: string
-) {
-//unsure if client is needed here.
-const client = twilio(keySid, secret, { accountSid })
-
+async function chatToken() {
 // Create a "grant" which enables a client to use Chat as a given user,
 const chatGrant = new ChatGrant({
   serviceSid: serviceSid,
@@ -32,17 +24,19 @@ const token = new AccessToken(
   accountSid,
   keySid,
   secret,
-  {identity: identity}
+  { identity: identity }
 );
 
-token.addGrant(chatGrant);
-
+token.addGrant(chatGrant); 
 // Serialize the token to a JWT string
 console.log(token.toJwt());
+return token.toJwt();
+}
+//complete module export
+module.exports = {chatToken};
 
 //return token as string
-return {
-  jwt: token.toJwt(),
-  identity
-};
-}
+//return {
+//  jwt: token.toJwt(),
+//  identity
+//};

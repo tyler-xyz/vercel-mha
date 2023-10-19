641c0a654dc5cd335f282885c70ce8e30271950a
tyler-xyz <77706863+tyler-xyz@users.noreply.github.com>
2023-10-19 03:56:00 +0000

    btn config

--- components/home/chat-button.tsx
+++ components/home/chat-button.tsx
@@ -1,12 +1,15 @@
+'use client'
 import { Button } from "@twilio-paste/button";
 import Link from "next/link";
 import { Letschat } from "../shared/icons";
 
 export default function LetsChat() {
   return(
-  <Link href='/chat' passHref>
+  <Link href='/chat' passHref
+  className="group flex max-w-fit items-center justify-center space-x-2 rounded-full border border-red bg-red px-5 py-2 text-sm text-white transition-colors hover:bg-white hover:text-red"
+  >
    <Button variant="destructive" size="rounded_small">
-    Let&apos;s Chat
+    Let&apos;s Chat 
     <Letschat />
    </Button>
   </Link>
--- components/shared/icons/letschat.tsx
+++ components/shared/icons/letschat.tsx
@@ -7,6 +7,7 @@ export default function LetsChatIcon({ className }: { className?: string }) {
       stroke-width="1.5" 
       stroke="currentColor" 
       className="w-5 h-5">
+      
 
         <path 
           stroke-linecap="round" 

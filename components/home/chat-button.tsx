273c22a96ebca8d22d720628d2359cd3b8de1f6a
tyler-xyz <77706863+tyler-xyz@users.noreply.github.com>
2023-10-19 04:34:08 +0000

    span1

--- components/home/chat-button.tsx
+++ components/home/chat-button.tsx
@@ -9,7 +9,7 @@ export default function LetsChat() {
   className="group flex max-w-fit items-center justify-center space-x-2 rounded-full border border-red bg-red px-5 py-2 text-sm text-white transition-colors hover:bg-white hover:text-red"
   >
    <Button variant="destructive" size="rounded_small">
-   <span className="space-x-5">
+   <span className="space-x-1">
           Let&apos;s Chat 
           <Letschat />
         </span>

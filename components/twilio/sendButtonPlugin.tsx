5b099ceba7d9e54630b5ee35dd0d0a3ad9e53a41
tyler-xyz <77706863+tyler-xyz@users.noreply.github.com>
2023-10-23 21:05:40 +0000

    removed enterkey function

--- components/twilio/sendButtonPlugin.tsx
+++ components/twilio/sendButtonPlugin.tsx
@@ -14,8 +14,7 @@ export const SendButtonPlugin = ({ onClick = () => {}}): JSX.Element => {
   const handleSend = (): void => {
     onClick();
    
-    // editor.dispatchCommand(CLEAR_EDITOR_COMMAND, undefined);
-
+    // editor update clear root text input
     editor.update(() => {
       $getRoot().clear();
     });
@@ -23,17 +22,10 @@ export const SendButtonPlugin = ({ onClick = () => {}}): JSX.Element => {
 
   return (
     <Box position="absolute" top="space30" right="space30">
-      <Button variant="primary_icon" size="reset" onClick={handleSend}>
+      <Button variant="primary_icon" size="reset" 
+      onClick={handleSend}>
         <SendIcon decorative={false} title="Send message" />
       </Button>
-      {/* <input
-        type="text"
-        onKeyDown={(event) => {
-          if (event.key === 'Enter') {
-            handleSend();
-          }
-        }}
-        /> */}
     </Box>
   );
 };

4f4f61ba3278e9a9c30a31ea2582a9e1cf6129d3
tyler-xyz <77706863+tyler-xyz@users.noreply.github.com>
2023-10-23 21:14:03 +0000

    assessment

--- components/home/chat-button.tsx
+++ components/home/chat-button.tsx
@@ -2,13 +2,59 @@
 import { Button } from "@twilio-paste/button";
 import Link from "next/link";
 import { Letschat } from "../shared/icons";
-
+import { useCallback, useState } from "react";
+import { useReCaptcha } from "next-recaptcha-v3";
 export default function LetsChat() {
+  const { executeRecaptcha } = useReCaptcha();
+  const [notification, setNotification] = useState<string>('');
+  const [notificationType, setNotificationType] = useState<string>('');
+
+  const handleSumitForm = useCallback(
+    (e: any) => {
+      e.preventDefault();
+      if (!executeRecaptcha) {
+        console.log("Execute recaptcha not yet available");
+        return;
+      }
+      executeRecaptcha("enquiryFormSubmit").then((gReCaptchaToken) => {
+        console.log(gReCaptchaToken, "response Google reCaptcha server");
+        submitEnquiryForm(gReCaptchaToken);
+      });
+    },
+    [executeRecaptcha]
+  );
+
+  const submitEnquiryForm = async (gReCaptchaToken: any) => {
+    fetch("/app/api/robot.ts", {
+      method: "POST",
+      headers: {
+        Accept: "application/json, text/plain, */*",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        gRecaptchaToken: gReCaptchaToken,
+      }),
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        console.log(res, "response from backend");
+        if (res?.status === "success") {
+          setNotification(res?.message);
+          setNotificationType(res?.status);
+        } else {
+          setNotification(res?.message);
+          setNotificationType(res?.status);
+        }
+      });
+  };
+
   return(
   <Link href='/chat' passHref
   className="group flex max-w-fit items-center justify-center space-x-2 rounded-full border border-red bg-red px-5 py-2 text-sm text-white transition-colors hover:bg-white hover:text-red"
   >
-   <Button variant="destructive" size="rounded_small">
+   <Button variant="destructive" size="rounded_small"
+      onClick={handleSumitForm}
+   >
     Let&apos;s Chat&nbsp; 
     <Letschat />
    </Button>

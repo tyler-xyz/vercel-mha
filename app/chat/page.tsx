9dbfa972c4868d6d8b444e2c2cb0e0d7610405fc
techezerteam <145917529+techezerteam@users.noreply.github.com>
2023-10-31 16:36:46 +0530

    Update page.tsx

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -6,31 +6,41 @@ import Loading from "@/components/home/loading-ui";
 import { Suspense } from "react";
 import { App } from "@/components/twilio/app";
 import Head from "next/head";
+import {useNavigate, useLocation} from "react-router-dom";
 import Script from "next/script";
 import "./styles.css";
 /* this is the index page for the Chat Page */
 
 export default function Chat() {
-  const [isLoaded, setIsLoaded] = useState<boolean>(false);
-  useEffect(() => {
-  setIsLoaded(true);
-  }, []);
-  
-  
-  
-  useEffect(() => {
-  if(isLoaded){
-  setTimeout(() => {
-  const script = document.createElement("script");
-  script.src = "./chat.js";
-  document.body.appendChild(script);
-  }, 1000);
-  }
-  }, [isLoaded]);
-  return (
-  <>
-  <Script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossOrigin="anonymous"/>
-  <Script src="https://assets.flex.twilio.com/releases/flex-webchat-ui/2.9.3/twilio-flex-webchat.min.js" integrity="sha512-lL9Ihx5WpKGmP1wzno3O9BMWAnKJDxfNfoE7/HnVfESbtBAzA6jUhAOU+b4Nq3WvZthSf7mOH3SNo7+zVP7BVQ==" crossOrigin=""/>
-  </>
-  );
-  }
+
+let script: any;
+const [isLoaded, setIsLoaded] = useState<boolean>(false);
+useEffect(() => {
+setIsLoaded(true);
+return () => {
+var tag1 = document.getElementById("twilio-script");
+var tag2 = document.getElementById("twilio-customer-frame");
+if(tag1){
+tag1.parentNode?.removeChild(tag1);
+tag2?.parentNode?.removeChild(tag2);
+}
+}
+}, []);
+
+useEffect(() => {
+if(isLoaded){
+setTimeout(() => {
+script = document.createElement("script");
+script.src = "./chat.js";
+script.id = "twilio-script"
+document.body.appendChild(script);
+}, 1000);
+}
+}, [isLoaded]);
+return (
+<>
+<Script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossOrigin="anonymous"/>
+<Script src="https://assets.flex.twilio.com/releases/flex-webchat-ui/2.9.3/twilio-flex-webchat.min.js" integrity="sha512-lL9Ihx5WpKGmP1wzno3O9BMWAnKJDxfNfoE7/HnVfESbtBAzA6jUhAOU+b4Nq3WvZthSf7mOH3SNo7+zVP7BVQ==" crossOrigin=""/>
+</>
+);
+}

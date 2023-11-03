f549142cc340598cc527e649f547356410289fa2
tyler-xyz <t13productions@gmail.com>
2023-11-02 20:52:24 -0400

    re-deploy test, style changes.

--- public/chat.js
+++ public/chat.js
@@ -5,13 +5,13 @@
 
 //  color theme 
 
-const brandColorTwilio1 = "#1976d2";
-const brandColorTwilio2 = "#233659";
+const brandColorTwilio1 = "#009DDC";
+const brandColorTwilio2 = "#2B3440";
 const brandTextColor = "#ffffff";
 
 
 const personalizedColors = {
-    darkBlueBackground: "#3C425C",
+    darkBlueBackground: "#62677c",
     whiteText: "#FFFFFF",
     entryPointBackground: "#3C425C",
     lighterBackground: "#ecedf1",
@@ -127,9 +127,9 @@ var appConfig = {
 Twilio.FlexWebChat.createWebChat(appConfig).then(webchat => {
     const { manager } = webchat;
     console.log(manager);
-    Twilio.FlexWebChat.MessagingCanvas.defaultProps.predefinedMessage.body = "Do you feel like you, or someone you know are in crisis? (yes / no)";
-	Twilio.FlexWebChat.MainHeader.defaultProps.titleText = ' ';
-	Twilio.FlexWebChat.MainHeader.defaultProps.showImage=false;
+    Twilio.FlexWebChat.MessagingCanvas.defaultProps.predefinedMessage.body = "Do you feel like you, or someone you know are in crisis? (Yes / No)";
+	Twilio.FlexWebChat.MainHeader.defaultProps.titleText = "Private Chat with a Youth Peer Advocate";
+	Twilio.FlexWebChat.MainHeader.defaultProps.showImage=true;
 	Twilio.FlexWebChat.MainHeader.defaultProps.imageUrl = "/favicon.ico";
     Twilio.FlexWebChat.Actions.on("afterStartEngagement", (payload) => {
         const { question } = payload.formData;
@@ -155,5 +155,45 @@ Twilio.FlexWebChat.createWebChat(appConfig).then(webchat => {
     
     }
     setTimeout(function() { $('.chat-loader').hide(); }, 3000);
+//11/2/23 start re-deploy test
+    // set time for session out - START
+
+    var set_min=1;
+    var set_time=set_min*60;
+    var timeout=set_time+2;
+
+    var i=0;
+
+    setInterval(function(){
+
+       console.log("time start");
+       localStorage.setItem("time_count", i);
+
+       var time_count=localStorage.getItem("time_count");
+
+       if(time_count ==set_time){
+
+           localStorage.setItem("time_count", timeout);
+           localStorage.clear();
+           alert("Your session has been ended due to inactivity");
+           location.reload();
+
+       }
+        i++;
+
+    },1000);
+
+
+
+     $('*').bind('click mousedown keypress change scroll resize dblclick mousemove', function (event ) {
+
+      console.log("active");
+      i=0;
+      localStorage.setItem("time_count", i);
+
+
+    });
+
+// set time for session out - END
 
 });

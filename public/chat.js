580acea98b380a323baee0e43c1a9bc91b478cca
tyler-xyz <t13productions@gmail.com>
2024-01-19 13:12:40 -0500

    switch flex and acctsid

--- public/chat.js
+++ public/chat.js
@@ -100,12 +100,12 @@ const brandedColors = {
 };
 const randomName = "User" + Math.floor(Math.random() * 1000);
 var appConfig = {
-    accountSid: "ACa3fc06fd70fef1a4fef034c857fdee2a",
+    //accountSid: "ACa3fc06fd70fef1a4fef034c857fdee2a",
 		//new acountSid below - JT1 Chat Line
- // accountSid: "ACa3fc06fd70fef1a4fef034c857fdee2a",
-    flexFlowSid: "FO6c3f7c21446e5f46c216a7c9fa9d67bb",
+    accountSid: "ACf74ee0b228033226bdea2f37a91390e7",
+    //flexFlowSid: "FO6c3f7c21446e5f46c216a7c9fa9d67bb",
     //new account flexflow below - JT1 Chat Line
-    //flexFlowSid: "FO59479fcca707f6160055e8e0c8badc0b",
+    flexFlowSid: "FO59479fcca707f6160055e8e0c8badc0b",
     componentProps: {
         MessagingCanvas: {
             memberDisplayOptions: {

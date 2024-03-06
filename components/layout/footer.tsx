51ee7607306a78df63a41f818da907361f840531
tyler-xyz <77706863+tyler-xyz@users.noreply.github.com>
2024-03-06 03:39:05 +0000

    footer

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -7,14 +7,14 @@ export default function Footer() {
   return (
     <div className="absolute w-full border-t border-gray-200 bg-opacity-100 bg-purple py-5 footer-mha bottom-0 text-center">
       <p className="font-low text-white">
-        Need Additional Help?{" "}
+        Need Additional Help? Find more on our {" "}
         <a
           className="font-low text-white underline transition-colors inline-flex"
           href="https://mhawny.org/jt1/about/"
           target="_self"
           rel="noopener noreferrer"
         >
-          Find more on our website 
+          website 
           <svg 
             xmlns="http://www.w3.org/2000/svg" 
             fill="none" 

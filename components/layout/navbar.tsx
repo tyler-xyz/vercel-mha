79b81ea1e70adef3c1fff30ec42a860e92693070
tyler-xyz <77706863+tyler-xyz@users.noreply.github.com>
2024-03-06 07:33:35 +0000

    another resize

--- components/layout/navbar.tsx
+++ components/layout/navbar.tsx
@@ -26,8 +26,8 @@ export default function NavBar({ session }: { session: Session | null }) {
             <Image
               src="/logo.png"
               alt="JustTellOne logo"
-              width="90"
-              height="70"
+              width="80"
+              height="62"
               className="mr-2 rounded-sm"
             >
             </Image>

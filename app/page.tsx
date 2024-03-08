6f053015c8192f9f78399eb3cf1540305a2a6013
tyler-xyz <77706863+tyler-xyz@users.noreply.github.com>
2024-03-08 16:49:46 +0000

    change to margin top

--- app/page.tsx
+++ app/page.tsx
@@ -54,7 +54,7 @@ export default async function Home() {
        {/* navigation warning  */}
         {/*<NavWarning />*/}
        <div
-          className="mx-auto mt-6 flex animate-fade-up items-center justify-center space-x-5 opacity-0"
+          className="mx-auto mt-1 flex animate-fade-up items-center justify-center space-x-5 opacity-0"
           style={{ animationDelay: "0.25s", animationFillMode: "forwards" }}
       >
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="stroke-white shrink-0 w-5 h-5"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>

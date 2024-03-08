e507c649c369c35c046b62125c87f8d6305f498b
tyler-xyz <77706863+tyler-xyz@users.noreply.github.com>
2024-03-08 15:38:58 +0000

    font display on all elements

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -6,10 +6,10 @@ const styles = {
 export default function Footer() {
   return (
     <div className="absolute w-full border-t border-gray-200 bg-opacity-100 bg-blue py-5 footer-mha bottom-0 text-center">
-      <p className="font-low text-white">
+      <p className="font-low font-display text-white">
         Need Additional Help? Find more on our {" "}
         <a
-          className="font-low text-white underline transition-colors inline-flex"
+          className="font-low font-display text-white underline transition-colors inline-flex"
           href="https://mhawny.org/jt1/about/"
           target="_self"
           rel="noopener noreferrer"

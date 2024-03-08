2f324e266ef3dc0eb50fa75217bd51a7857d2de0
tyler-xyz <77706863+tyler-xyz@users.noreply.github.com>
2024-03-08 16:07:21 +0000

    font size 10px

--- components/home/nav-warning.tsx
+++ components/home/nav-warning.tsx
@@ -5,7 +5,7 @@ export default function NavWarning() {
     return(
         <div className="alert alert-info bg-indigo-500/50 mx-auto mt-0 flex animate-fade-up items-center justify-center space-x-5 " style={{ minHeight: 'auto', display: 'inline-block' }}>
            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="stroke-current shrink-0 w-6 h-6"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
-            <p className="md:text-xxsm font-display text-sm">Please note that while you chat with us you must remain on the app. If you navigate away from the chat window your connection may be lost.</p>
+            <p className="md:text-xxsm font-display style={{ fontSize: '10px' }}">Please note that while you chat with us you must remain on the app. If you navigate away from the chat window your connection may be lost.</p>
         </div> 
     )
 }
